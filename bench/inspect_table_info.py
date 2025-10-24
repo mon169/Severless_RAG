ed6e4c4e9c81e1f1f7e1e4dc7e1c8ac50f66302f
mon169 <mon169@naver.com>
2025-10-25 06:56:27 +0900

    코드 개선, Read.me에 필요한 이미지 추가

--- bench/inspect_table_info.py
+++ bench/inspect_table_info.py
@@ -3,7 +3,7 @@ import lancedb
 # 주석 해제 하고 올바른 주소 넣을 것 db = lancedb.connect("")
 
 # 올바른 테이블 열기
-tbl = db.open_table("chunks_dedup")
+# 주석 해제 하고 올바른 주소 넣을 것 tbl = db.open_table("")
 
 print("Schema:", tbl.schema)
 print("Row count:", tbl.count_rows())
